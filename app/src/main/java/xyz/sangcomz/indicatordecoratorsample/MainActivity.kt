d34725e0ceb1faa059acd87d3c4b47fbc8dafcd2
sangcomz <sangcomz@naver.com>
2019-09-27 23:35:07 +0900

    Fix Sample App

--- app/src/main/java/xyz/sangcomz/indicatordecoratorsample/MainActivity.kt
+++ app/src/main/java/xyz/sangcomz/indicatordecoratorsample/MainActivity.kt
@@ -12,16 +12,42 @@ import xyz.sangcomz.indicatordecorator.shape.SquareIndicator
 
 class MainActivity : AppCompatActivity() {
 
-    private val adapter: ListAdapter = ListAdapter(
+    private val adapter1: ListAdapter = ListAdapter(
         listOf(
-            R.color.page1 to "page 1",
-            R.color.page2 to "page 2",
-            R.color.page3 to "page 3",
-            R.color.page4 to "page 4",
-            R.color.page5 to "page 5",
-            R.color.page6 to "page 6",
-            R.color.page7 to "page 7",
-            R.color.page8 to "page 8"
+            R.color.page1 to "DrawableIndicator\npage 1",
+            R.color.page2 to "DrawableIndicator\npage 2",
+            R.color.page3 to "DrawableIndicator\npage 3",
+            R.color.page4 to "DrawableIndicator\npage 4",
+            R.color.page5 to "DrawableIndicator\npage 5",
+            R.color.page6 to "DrawableIndicator\npage 6",
+            R.color.page7 to "DrawableIndicator\npage 7",
+            R.color.page8 to "DrawableIndicator\npage 8"
+        )
+    )
+
+    private val adapter2: ListAdapter = ListAdapter(
+        listOf(
+            R.color.page8 to "SquareIndicator\npage 1",
+            R.color.page7 to "SquareIndicator\npage 2",
+            R.color.page6 to "SquareIndicator\npage 3",
+            R.color.page5 to "SquareIndicator\npage 4",
+            R.color.page4 to "SquareIndicator\npage 5",
+            R.color.page3 to "SquareIndicator\npage 6",
+            R.color.page2 to "SquareIndicator\npage 7",
+            R.color.page1 to "SquareIndicator\npage 8"
+        )
+    )
+
+    private val adapter3: ListAdapter = ListAdapter(
+        listOf(
+            R.color.page4 to "CircleIndicator\npage 1",
+            R.color.page5 to "CircleIndicator\npage 2",
+            R.color.page6 to "CircleIndicator\npage 3",
+            R.color.page7 to "CircleIndicator\npage 4",
+            R.color.page8 to "CircleIndicator\npage 5",
+            R.color.page1 to "CircleIndicator\npage 6",
+            R.color.page2 to "CircleIndicator\npage 7",
+            R.color.page3 to "CircleIndicator\npage 8"
         )
     )
 
@@ -29,17 +55,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewPager1.adapter = adapter
+        viewPager1.adapter = adapter1
         viewPager1.addItemDecoration(IndicatorItemDecoration().apply {
-            indicatorShape = CircleIndicator()
+            indicatorShape = CircleIndicator().apply {
+                colorActive = ContextCompat.getColor(this@MainActivity, R.color.colorPrimaryDark)
+            }
         })
 
-        viewPager2.adapter = adapter
+        viewPager2.adapter = adapter2
         viewPager2.addItemDecoration(IndicatorItemDecoration().apply {
-            indicatorShape = SquareIndicator()
+            indicatorShape = SquareIndicator().apply {
+                colorActive = ContextCompat.getColor(this@MainActivity, R.color.colorPrimaryDark)
+            }
         })
 
-        recyclerView.adapter = adapter
+        recyclerView.adapter = adapter3
         PagerSnapHelper().attachToRecyclerView(recyclerView)
         recyclerView.addItemDecoration(IndicatorItemDecoration().apply {
             indicatorShape = DrawableIndicator(
@@ -47,8 +77,10 @@ class MainActivity : AppCompatActivity() {
                     this@MainActivity,
                     R.drawable.ic_grade_red_24dp
                 )!!,
-                ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_grade_black_24dp)!!
-            )
+                ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_grade_gray_24dp)!!
+            ).apply {
+                width = 64f
+            }
         })
 
 
